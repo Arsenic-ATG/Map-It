aa3ba4a51be6b510cf68a541a205971dbf2802c1
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2020-08-06 16:35:01 +0530

    📝 Lay down the basic layout

---
+++ MapIt.py
@@ -0,0 +1,11 @@
+
+# mapIt.py - Launches a map in the browser using an address from the command line or clipboard.
+ 
+import webbrowser,sys
+
+if len(sys.argv) > 1
+	# TODO: Get length from command line
+
+# TODO: get address from clipboard
+
+# TODO: use webbrwoser to open thet webpage
