e124afa8c62f9f9f3401920b3a8c511d51d68cdd
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2020-08-06 16:44:07 +0530

    ➕ Copy content from clipboard

--- MapIt.py
+++ MapIt.py
@@ -1,12 +1,14 @@
 
 # mapIt.py - Launches a map in the browser using an address from the command line or clipboard.
  
-import webbrowser,sys
+import webbrowser,sys,pyperclip
 
 if len(sys.argv) > 1
 	# Get length from command line
 	address = ' '.join(sys.argv[1:])
 
-# TODO: get address from clipboard
+else
+	# Get address from clipboard
+	address = pyperclip.paste()
 
 # TODO: use webbrwoser to open thet webpage
